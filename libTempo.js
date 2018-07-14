886cc4d1d0c5194c001297171d3ad9ff114d29ac
Ana Losnak <ana.losnak@caelum.com.br>
2018-07-14 13:50:21 -0300

    Arrumando código

--- libTempo.js
+++ libTempo.js
@@ -1,6 +1,6 @@
-var tempoMinutos;
+var tempoSoComMinutos;
 function formataRelogio(){
-    var tempoEmHorasFloat = tempoMinutos/60
+    var tempoEmHorasFloat = tempoSoComMinutos/60
     var horas = parseInt(tempoEmHorasFloat)
     var minutos = parseInt((tempoEmHorasFloat - horas) * 60)
     return horas.toString().padStart(2,0) + ":" + minutos.toString().padStart(2,0)
